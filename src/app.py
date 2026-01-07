5b7f5c56512a23208fb8aec1c9be1019dc00c68e
Jacob Diamond <jake.b.diamond@gmail.com>
2026-01-06 17:32:53 -0700

    Updated Python logging syntax and updated image to v.5.0.0.

--- src/app.py
+++ src/app.py
@@ -9,5 +9,5 @@ app.logger.setLevel(logging.INFO)
 @app.route("/")
 def hello_world():
     # custom logging should print to the terminal in --debug mode (but would be grabbed automatically by a logging agent like Fluentd)
-    app.logger.info(">>> SUCCESS: Ingress hit the Flask app! <<<") 
+    app.logger.info(">>> SUCCESS: Ingress hit the Flask app!!!!! <<<") 
     return "<p>Hello, World!</p>" # returns a simply HTML-formatted "Hello, World!" on a HTTP GET request
